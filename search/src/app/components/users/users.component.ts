80344be41a2f95f0f20858a6ed11563dd21da150
laurettemongina <monginalaurette2gmail.com>
2021-08-30 22:51:11 +0300

    IMports user and repo class.

---
+++ search/src/app/components/users/users.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../usertwo.service';
+import { User } from '../user';
+import { Repo } from '../repo';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  repo: any;
+  user: any;
+  constructor(public myService: UserService) { }
+
+  getUsers(userName) {
+    this.myService.getUsers(userName).then(
+      (success) => {
+        this.user = this.myService.user;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+      this.myService.getRepo(userName).then(
+        (success)=>{
+          this.repo =this.myService.repo;
+          console.log(this.repo);
+        },
+        (error)=>{
+          console.log(error);
+        }
+      )
+  }
+
+  ngOnInit(): void {
+    this.getUsers("laurettemongina")
+  }
+}
