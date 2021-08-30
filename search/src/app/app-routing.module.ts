e73688febef93a7de689d8a07f794d15ccb349ec
laurettemongina <monginalaurette2gmail.com>
2021-08-30 22:51:34 +0300

    Creates path for routing.

--- search/src/app/app-routing.module.ts
+++ search/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from "./components/search/search.component";
+import { UsersComponent } from "./components/users/users.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"users",component:UsersComponent},
+  {path:"search",component:SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
